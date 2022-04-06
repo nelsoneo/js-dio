7a097b5f164cd2c3ddad4e98731e1c7b3e7f346d
nelsoneo <nelsoneo@gmail.com>
2022-04-05 23:26:36 -0300

    module

---
+++ modulo/funcoes.js
@@ -0,0 +1,13 @@
+function mostraIdade(nome, idade) {
+  return `A idade de ${nome} é ${idade}`;
+}
+
+function mostraCidade(cidade, nome) {
+  return `A cidade de ${nome} é ${cidade}`;
+}
+
+function mostraHobby(hobby, nome) {
+  return `A cidade de ${nome} é ${hobby}`;
+}
+
+export { mostraIdade, mostraCidade, mostraHobby };
